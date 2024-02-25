559408912ea75a178d5dae05782d27854ecbe43c
kosmosa017 <sandromeshveliani61@gmail.com>
2024-02-25 15:15:59 +0400

    first commit

---
+++ Day 1/lesson1.py
@@ -0,0 +1,16 @@
+print("mevar")
+name = "sandro"  #ეს არის (string) ტიპის ცვლადი
+surname = "meshveliani"
+age = 22  # ეს არის int (integer) მთელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+
+knows_programming = True  #True ან False
+is_ugly = False  #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+print (name + " " + surname + " " + str(age)  + " " + str(height))
+
+
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
